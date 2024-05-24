a88a11214046603888a508fdcdb68a79a71c03f3
fixx <fixx@hotmail.de>
2024-05-24 19:50:53 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace FixedRectToolForButano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCenterX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCenterY = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeigth = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxImgHeigth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxTileIndex = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxCenterX
+            // 
+            this.textBoxCenterX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCenterX.Location = new System.Drawing.Point(503, 71);
+            this.textBoxCenterX.Name = "textBoxCenterX";
+            this.textBoxCenterX.Size = new System.Drawing.Size(92, 20);
+            this.textBoxCenterX.TabIndex = 1;
+            this.textBoxCenterX.Text = "0";
+            this.textBoxCenterX.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Center X";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Center Y";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(439, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(439, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Height";
+            // 
+            // textBoxCenterY
+            // 
+            this.textBoxCenterY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCenterY.Location = new System.Drawing.Point(503, 95);
+            this.textBoxCenterY.Name = "textBoxCenterY";
+            this.textBoxCenterY.Size = new System.Drawing.Size(92, 20);
+            this.textBoxCenterY.TabIndex = 1;
+            this.textBoxCenterY.Text = "0";
+            this.textBoxCenterY.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxWidth.Location = new System.Drawing.Point(503, 121);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(92, 20);
+            this.textBoxWidth.TabIndex = 1;
+            this.textBoxWidth.Text = "0";
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // textBoxHeigth
+            // 
+            this.textBoxHeigth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxHeigth.Location = new System.Drawing.Point(503, 147);
+            this.textBoxHeigth.Name = "textBoxHeigth";
+            this.textBoxHeigth.Size = new System.Drawing.Size(92, 20);
+            this.textBoxHeigth.TabIndex = 1;
+            this.textBoxHeigth.Text = "0";
+            this.textBoxHeigth.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(77, 72);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBoxImgHeigth
+            // 
+            this.textBoxImgHeigth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxImgHeigth.Location = new System.Drawing.Point(503, 22);
+            this.textBoxImgHeigth.Name = "textBoxImgHeigth";
+            this.textBoxImgHeigth.Size = new System.Drawing.Size(92, 20);
+            this.textBoxImgHeigth.TabIndex = 1;
+            this.textBoxImgHeigth.Text = "32";
+            this.textBoxImgHeigth.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(439, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Img. Height";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(439, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Tile Index";
+            // 
+            // textBoxTileIndex
+            // 
+            this.textBoxTileIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTileIndex.Location = new System.Drawing.Point(503, 48);
+            this.textBoxTileIndex.Name = "textBoxTileIndex";
+            this.textBoxTileIndex.Size = new System.Drawing.Size(92, 20);
+            this.textBoxTileIndex.TabIndex = 1;
+            this.textBoxTileIndex.Text = "0";
+            this.textBoxTileIndex.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(12, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(406, 419);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 440);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxHeigth);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxCenterY);
+            this.Controls.Add(this.textBoxTileIndex);
+            this.Controls.Add(this.textBoxImgHeigth);
+            this.Controls.Add(this.textBoxCenterX);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FixedRect Tool for Butano";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxCenterX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCenterY;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeigth;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxImgHeigth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxTileIndex;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FixedRectToolForButano
+{
+    public partial class Form1 : Form
+    {
+        int sizeMultiplier = 4;
+        Bitmap bmp = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) 
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                try
+                {
+                    bmp = new Bitmap(file);
+                    panel1.Width = bmp.Width * sizeMultiplier;
+                    panel1.Height = bmp.Height * sizeMultiplier;
+                    textBoxImgHeigth.Text = bmp.Width.ToString();
+                    panel1.Refresh();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bmp != null)
+            {
+                {
+                    e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                    int width = bmp.Width * sizeMultiplier;
+                    int height = bmp.Height * sizeMultiplier;
+                    e.Graphics.DrawImage(
+                        bmp,
+                        new Rectangle(0, 0, width, height),  // destination rectangle
+                        0,
+                        0,           // upper-left corner of source rectangle
+                        bmp.Width,       // width of source rectangle
+                        bmp.Height,      // height of source rectangle
+                        GraphicsUnit.Pixel,
+                        null);
+
+                }
+                try
+                {
+                    Brush brush = new HatchBrush(HatchStyle.DottedGrid, Color.Black);
+                    Pen pen = new Pen(brush);
+                    pen.Width = 1;
+
+                    float imgHeigth = float.Parse(textBoxImgHeigth.Text);
+                    float bbCenterX = float.Parse(textBoxCenterX.Text) * sizeMultiplier;
+                    float bbCenterY = float.Parse(textBoxCenterY.Text) * sizeMultiplier;
+                    float bbWidth = float.Parse(textBoxWidth.Text) * sizeMultiplier;
+                    float bbHeight = float.Parse(textBoxHeigth.Text) * sizeMultiplier;
+                    float tileIndex = float.Parse(textBoxTileIndex.Text);
+
+                    float centerX = (bmp.Width * sizeMultiplier) / 2f;
+                    float centerY = (imgHeigth * sizeMultiplier) / 2f;
+                    
+
+                    centerX += bbCenterX;
+                    centerY += bbCenterY;
+
+                    float x = centerX - bbWidth / 2f;
+                    float y = centerY - bbHeight / 2f + (tileIndex * imgHeigth * sizeMultiplier);
+
+
+
+                    e.Graphics.DrawRectangle(pen, x, y, bbWidth, bbHeight);
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+    }
+}
